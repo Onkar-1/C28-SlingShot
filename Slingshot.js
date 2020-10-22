ff8232e8900d52db77580f94be1af80d582492cb
Onkar-1 <71157521+Onkar-1@users.noreply.github.com>
2020-10-21 20:39:19 -0400

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,29 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB;
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+//fly so that bodya is attached to nothing
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+
+    display(){
+        //if this.sling.bodyA means if the body a is in the constraint
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+}
